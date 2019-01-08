1effebf0c807ce3b3575120eff20840258395a62
Tracie Masek <traciemasek@gmail.com>
2019-01-08 13:00:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Add your doToElementsInArray() function here:
-
+const doToElementsInArray = (element, callback) => {
+  arr.forEach(callback);
+}
 
 // Add your changeCompletely() function here:
